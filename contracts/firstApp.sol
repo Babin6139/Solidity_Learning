fbf194890704cc43a014fb9704e901ffae3e59e7
Babin6139 <khatribabin39@gmail.com>
2023-02-23 16:33:19 +0000

    learning: how to write comments

--- contracts/firstApp.sol
+++ contracts/firstApp.sol
@@ -6,6 +6,11 @@ contract SolidityTest{
     }
 
     function getResult() public view returns (uint){
+        //This is a comment
+
+        /*
+        This is a multi line comment
+        */
         uint a =1;
         uint b =2;
         uint result= a + b;
